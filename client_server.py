48bd159e18bb92cb85b88f22a75dc09540f4ad3d
LIJYann <394598772@qq.com>
2018-04-18 11:38:36 +0800

    Add files via upload

---
+++ client_server.py
@@ -0,0 +1,62 @@
+#import necessary package
+import socket
+import time
+import sys
+import RPi.GPIO as GPIO
+
+#define host ip: Rpi's IP
+HOST_IP = "192.168.1.106"
+HOST_PORT = 8888
+print("Starting socket: TCP...")
+
+#1.create socket object:socket=socket.socket(family,type)
+socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("TCP server listen @ %s:%d!" %(HOST_IP, HOST_PORT) )
+host_addr = (HOST_IP, HOST_PORT)
+
+#2.bind socket to addr:socket.bind(address)
+socket_tcp.bind(host_addr)
+
+#3.listen connection request:socket.listen(backlog)
+socket_tcp.listen(1)
+
+#4.waite for client:connection,address=socket.accept()
+socket_con, (client_ip, client_port) = socket_tcp.accept()
+print("Connection accepted from %s." %client_ip)
+
+
+#5.handle
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+print("Receiving package...")
+
+#RPi's IP
+SERVER_IP = "192.168.1.106"
+SERVER_PORT = 8888
+
+#connect to the server pi
+server_addr = (SERVER_IP, SERVER_PORT)
+
+while True:
+    try:
+        print("Connecting to server @ %s:%d..." %(SERVER_IP, SERVER_PORT))
+        socket_tcp.connect(server_addr)
+        break
+    except Exception:
+        print("Can't connect to server,try it latter!")
+        time.sleep(1)
+        continue
+
+
+
+while True:
+    try:
+        data=socket_con.recv(512)
+        if len(data)>0:
+            print("Received:%s"%data)
+            socket_tcp.send(data)
+            continue
+    except Exception:
+            socket_tcp.close()
+            sys.exit(1)
+            
